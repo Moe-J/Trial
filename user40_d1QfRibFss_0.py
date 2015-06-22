065d8898e5f022206e0f1cb555c02e780a1d76dc
Moe-J <jubran.mohammad@gmail.com>
2015-06-22 16:30:20 +0400

    Added a file
    Just trying the Git Hub

---
+++ user40_d1QfRibFss_0.py
@@ -0,0 +1,19 @@
+# SimpleGUI program template
+
+# Import the module
+import simplegui
+
+# Define global variables (program state)
+
+# Define "helper" functions
+
+# Define classes
+
+# Define event handler functions
+
+# Create a frame
+
+# Register event handlers
+
+# Start frame and timers
+
